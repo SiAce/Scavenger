aed3795d75e57bc0d458cf6292da6ac772fe1e36
SiAce <siacespark@gmail.com>
2019-01-28 13:56:21 +0800

    Initial commit

---
+++ Scripts/GameManager.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public float levelStartDelay = 2f;
+    public float turnDelay = 0.05f;
+    public static GameManager instance = null;
+    [HideInInspector] public int playerFoodPoints = 100;
+    [HideInInspector] public bool playersTurn = true;
+    [HideInInspector] public int level = 1;
+
+    private BoardManager boardScript;
+    private Text levelText;
+    private GameObject levelImage;
+    private GameObject restartButton;
+    private List<Enemy> enemies;
+    private bool enemiesMoving;
+    private bool doingSetup = true;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+        enemies = new List<Enemy>();
+        boardScript = GetComponent<BoardManager>();
+
+
+        InitGame();
+    }
+
+    private void OnLevelWasLoaded(int index)
+    {
+        level++;
+
+        InitGame();
+    }
+
+    void InitGame()
+    {
+        doingSetup = true;
+
+        levelImage = GameObject.Find("LevelImage");
+        levelText = GameObject.Find("LevelText").GetComponent<Text>();
+        restartButton = GameObject.Find("RestartButton");
+        restartButton.SetActive(false);
+        levelText.text = "Day " + level;
+        levelImage.SetActive(true);
+        
+        Invoke("HideLevelImage", levelStartDelay);
+
+        enemies.Clear();
+        boardScript.SetupScene(level);
+    }
+
+    private void HideLevelImage()
+    {
+        levelImage.SetActive(false);
+        doingSetup = false;
+    }
+
+    public void GameOver()
+    {
+        levelText.text = "After " + level + " days, you starved.";
+        levelImage.SetActive(true);
+        enabled = false;
+        StartCoroutine(ShowRestartButton());
+    }
+
+    IEnumerator ShowRestartButton()
+    {
+        yield return new WaitForSeconds(3f);
+        restartButton.SetActive(true);
+    }
+
+    private void Update()
+    {
+        if(playersTurn || enemiesMoving || doingSetup)
+        {
+            return;
+        }
+        StartCoroutine(MoveEnemies());
+    }
+
+    public void AddEnemyToList(Enemy script)
+    {
+        enemies.Add(script);
+    }
+
+    IEnumerator MoveEnemies()
+    {
+        enemiesMoving = true;
+        yield return new WaitForSeconds(turnDelay);
+        if(enemies.Count == 0)
+        {
+            yield return new WaitForSeconds(turnDelay);
+        }
+
+        for (int i = 0; i < enemies.Count; i++)
+        {
+            enemies[i].MoveEnemy();
+            yield return new WaitForSeconds(enemies[i].moveTime);
+        }
+
+        playersTurn = true;
+        enemiesMoving = false;
+    }
+}
